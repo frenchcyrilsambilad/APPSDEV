162a2ddd948a49906a044eb4aa847112d1595a87
French <123856224+frenchcyrilsambilad@users.noreply.github.com>
2025-03-17 23:28:08 +0800

    Add files via upload

---
+++ SambiladMidtermSeatwork/SambiladMidtermSeatwork/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SambiladMidtermSeatwork
+{
+    public partial class Form1 : Form
+    {
+        string currentInput = "";
+        double result = 0;
+        string operation = "";
+        bool operationPending = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Text;
+            textBox1.Text = currentInput;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Text;
+            textBox1.Text = currentInput;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            System.Data.DataTable table = new System.Data.DataTable();
+            try
+            {
+                table.Columns.Add("expression", string.Empty.GetType(), textBox1.Text);
+            }
+            catch (SyntaxErrorException ex)
+            {
+                textBox1.Text = "Invalid input";
+                return;
+            }
+            System.Data.DataRow row = table.NewRow();
+            table.Rows.Add(row);
+            if (row["expression"] != DBNull.Value && !string.IsNullOrEmpty((string)row["expression"]))
+            {
+                double result = double.Parse((string)row["expression"]);
+                textBox1.Text = result.ToString();
+            }
+            else
+            {
+                textBox1.Text = "Invalid input";
+            }
+        }
+
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Text;
+            textBox1.Text = currentInput;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Text;
+            textBox1.Text = currentInput;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Text;
+            textBox1.Text = currentInput;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Text;
+            textBox1.Text = currentInput;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Text;
+            textBox1.Text = currentInput;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Text;
+            textBox1.Text = currentInput;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Text;
+            textBox1.Text = currentInput;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Text;
+            textBox1.Text = currentInput;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Text;
+            textBox1.Text = currentInput;
+        }
+
+        private void Evaluate()
+        {
+            if (operationPending)
+            {
+                if (string.IsNullOrWhiteSpace(currentInput))
+                {
+                    MessageBox.Show("Please enter a number.");
+                    return;
+                }
+
+                double secondNumber = double.Parse(currentInput);
+
+                switch (operation)
+                {
+                    case "+":
+                        result += secondNumber;
+                        break;
+                    case "-":
+                        result -= secondNumber;
+                        break;
+                    case "*":
+                        result *= secondNumber;
+                        break;
+                    case "/":
+                        if (secondNumber != 0)
+                        {
+                            result /= secondNumber;
+                        }
+                        else
+                        {
+                            textBox1.Text = "Error";
+                            return;
+                        }
+                        break;
+                }
+
+                textBox1.Text = result.ToString();
+                currentInput = textBox1.Text;
+                operationPending = false;
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Text;
+            textBox1.Text = currentInput;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Text;
+            textBox1.Text = currentInput;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Text;
+            textBox1.Text = currentInput;
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            currentInput = "";
+            result = 0;
+            operation = "";
+            operationPending = false;
+            textBox1.Text = "";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            if (currentInput.Length > 0)
+            {
+                currentInput = currentInput.Substring(0, currentInput.Length - 1);
+                textBox1.Text = currentInput;
+            }
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(currentInput))
+            {
+                string[] parts = currentInput.Split(new char[] { '+', '-', '*', '/' });
+                string lastPart = parts.Length > 0 ? parts[parts.Length - 1] : "";
+
+                if (!lastPart.Contains("."))
+                {
+                    currentInput += ".";
+                    textBox1.Text = currentInput;
+                }
+            }
+        }
+    }
+}
